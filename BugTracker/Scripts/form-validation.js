5e1c7dbc902c7394dad6a3fb9650bc2ea10bd743
ALTUĞ CEYLAN <altug.ceylan@std.antalya.edu.tr>
2020-02-04 15:52:42 +0300

    Partial jquery form validation implemented

---
+++ BugTracker/Scripts/form-validation.js
@@ -0,0 +1,91 @@
+﻿// Wait for the DOM to be ready
+
+$(function () {  
+    // Initialize form validation on the registration form.
+    // It has the name attribute "create"
+    $("form[name='create']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            title: "required",
+            description: "required",
+            version: "required"
+        },
+        // Specify validation error messages
+        messages: {
+            title: "Please enter a title",
+            description: "Please enter a description",
+            version: "Please enter a version"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+    $("form[name='edit']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            title: "required",
+            description: "required",
+            version: "required"
+        },
+        // Specify validation error messages
+        messages: {
+            title: "Please enter a title",
+            description: "Please enter a description",
+            version: "Please enter a version"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+    $("form[name='asedit']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            password: "required"
+        },
+        // Specify validation error messages
+        messages: {
+            password: "Please enter a password"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+    $("form[name='ascreate']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            email: {
+                required: true,
+                email: true
+            },
+            password: "required"
+        },
+        // Specify validation error messages
+        messages: {
+            email: "Please enter a valid email address",
+            password: "Please enter a password"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
